640cb7c3794b3b5f03c60d7f6ef8e4b4d73d6456
DESKTOP-NVJO8FT\uxtuno <uxtuno664@gmail.com>
2019-03-07 02:08:07 +0900

    [Fixed] 初回コミット

---
+++ Assets/Scripts/RefarenceReplacer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RefarenceReplacer : EditorWindow
+{
+	[MenuItem("Window/Reference Replacer")]
+	static void Open()
+	{
+		var window = CreateInstance<RefarenceReplacer>();
+		window.Show();
+	}
+
+	Object targetObject;
+	Object replaceObject;
+
+	void OnGUI()
+	{
+		targetObject = EditorGUILayout.ObjectField("Target Object", targetObject, typeof(Object), true);	
+		replaceObject = EditorGUILayout.ObjectField("Replace Object", replaceObject, typeof(Object), true);
+
+		if (GUILayout.Button("Replace")) {
+			foreach (var gameObject in FindObjectsOfType<GameObject>()) {
+				foreach (var component in gameObject.GetComponents<MonoBehaviour>()) {
+					var serializedObject = new SerializedObject(component);
+					var iterator = serializedObject.GetIterator();
+					while(iterator.Next(true)) {
+						if (iterator.propertyType == SerializedPropertyType.ObjectReference) {
+							if (iterator.objectReferenceValue == targetObject) {
+								iterator.objectReferenceValue = replaceObject;
+							}
+						}
+					}
+					
+					serializedObject.ApplyModifiedProperties();
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TestScript1.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript1 : MonoBehaviour
+{
+	[SerializeField]
+	GameObject reference;
+}
